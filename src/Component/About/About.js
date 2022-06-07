21ce565d6ed9b7dacf00c9141e9c70e06e5ac435
Hasan Shanto <hasanshanto922@gmail.com>
2022-06-07 13:09:16 +0600

    about edited

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <h3>My Goals of Near Future</h3>
             <p>Hi, I am Hasan JHafizur Rahman an enthusiast developer currently learning Mern Stack development. Mern stands for MongoDB, Express Js , React JS,  AND Node js. It is a complete full stack development. althouth I mentioned few technologies here but I also know bootstarp, tailwind, Firebase etc.</p>
             <br />
-            <p>My focus for next few month is to learn more about my current working technology  . And I </p>
+            <p>My focus for next few month is to learn more about my current working technologies that will enhance my developement knowledge. </p>
         </div>
     );
 };
